279f195b7d57efdec52de43fb59f92b40d39896b
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-03-30 16:18:14 -0700

    This code checks to see if the person's inputed age qualifies for a movie discout using an if else statement and && and OR operators to specify the conditions to verify their age

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Excersize_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //17
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the movie rating");
+            //The whole thing need to be true or none of it is true 
+            //&& will take precedence will come first in the arguement/ condition
+            char rating = char.Parse(Console.ReadLine());
+            if (age <= 12 || age >= 55 && rating == 'G')
+            {
+                Console.WriteLine("Discount applies");
+            }
+            else
+            {//example input 15 and G then this code will execute because age>=55 && rating == 'G'are grouped first
+                Console.WriteLine(" The Discount does not apply here bucko");
+            }
+        }
+    }
+}
